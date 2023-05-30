bc5ed908db5acc520ab53ec4ef65e95490313ef2
Jallard <justinallard2001@gmail.com>
2023-05-29 22:11:57 -0400

    Add solution to q 226

---
+++ 226/mySolution.cpp
@@ -0,0 +1,45 @@
+#include <alloca.h>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (!root) {
+            return root;
+        }
+        TreeNode* temp = nullptr;
+        TreeNode* curr = nullptr;
+        int memSize = 10;
+        int memPos = 0;
+        TreeNode** mem = (TreeNode **)alloca(memSize * sizeof (TreeNode *));
+        mem[memPos++] = root;
+        while (memPos > 0) {
+            curr = mem[--memPos];
+            while (curr) {
+                temp = curr->right;
+                curr->right = curr->left;
+                curr->left = temp;
+                if (curr->right) {
+                    // push right node on stack
+                    if (memPos == memSize) {
+                        // this would work properly if alloca did as expected
+                        alloca(memSize * sizeof (TreeNode *));
+                        memSize += memSize;
+                    }
+                    mem[memPos++] = curr->right;
+                }
+                curr = curr->left;
+            }
+        }
+        return root;
+    }
+};
